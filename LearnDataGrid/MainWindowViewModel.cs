4051572696a8ee6457e3469aa6252d2021ba1f28
Faiz Ghazali Desktop <99084931+FaizGhazali@users.noreply.github.com>
2024-11-29 14:52:51 +0800

    Add project files.

---
+++ LearnDataGrid/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace LearnDataGrid;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+
+    public ObservableCollection<User> Users { get; } = new();
+
+    public MainWindowViewModel()
+    {
+        //generate 10 random users
+        var random = new Random();
+        Users.Add(item: new User { Name = "Joe", Age = 3 });
+        for (int i = 0; i < 10; i++)
+        {
+            Users.Add(item: new User { Name = $"User {i}", Age = random.Next(1, 100) });
+        }
+    }
+    [RelayCommand]
+    private void NewUser()
+    {
+        Users.Add(new User
+        {
+            Name = $"User {Users.Count}",
+            Age = 3
+        });
+    }
+    [RelayCommand]
+    private void DeleteUser(User user)
+    {
+        Users.Remove(user);
+    }
+
+}
---
+++ LearnDataGrid/User.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LearnDataGrid;
+
+public partial class User : ObservableObject
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    [ObservableProperty]
+    private string? _name;
+    [ObservableProperty]
+    private int? _age;
+
+   
+}
